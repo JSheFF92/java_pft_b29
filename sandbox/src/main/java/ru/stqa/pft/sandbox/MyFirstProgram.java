2aad8b0d3fc6cd923f2b5e861fb5b3628411f2b3
JSheFF92 <shevchuk_1992@list.ru>
2021-11-21 01:46:22 +0300

    устранение ошибки сборщика: "You can use '--warning-mode all' to show the individual deprecation warnings and determine if they come from your own scripts or plugins."  в build and run using

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, " + "world");
+        int l = 6;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
 }
