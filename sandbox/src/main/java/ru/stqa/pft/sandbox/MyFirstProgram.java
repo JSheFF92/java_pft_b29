f848c008695d86bd036f83ed8bd8154eec03d39c
JSheFF92 <shevchuk_1992@list.ru>
2021-11-21 13:18:06 +0300

    удалена папка build, поправлен гитигнор

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,10 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, " + "world");
-        int l = 6;
-        int s = l * l;
+        String somebody = "world";
+        System.out.println("Hello, " + somebody);
+        double l = 8.0;
+        double s = l * l;
         System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
