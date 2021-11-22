0785e841a26bec80a70d088c00faffda7fe846d8
JSheFF92 <shevchuk_1992@list.ru>
2021-11-22 21:25:56 +0300

    Добавил задание "Расстояние между точками на двумерной плоскости"

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double p1, double p2) {
+        this.x = p1;
+        this.y = p2;
+    }
+}
