98a4535a2a356e3d39dc7b7ebd0f1f27c4f8ded6
JSheFF92 <shevchuk_1992@list.ru>
2021-11-23 07:15:16 +0300

    Добавил положительный testPoin

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -8,14 +8,21 @@ import static ru.stqa.pft.sandbox.Distance.distance;
 
     public class PointTests {
         @Test
-        public void testPointTrue() {
+        public void testPoint1() {
             Point point1 = new Point(1, 4);
             Point point2 = new Point(2, 7);
             Assert.assertEquals(distance(point1, point2), 3.1622776601683795);
         }
 
         @Test
-        public void testPointNotTrue() {
+        public void testPoint2() {
+            Point point1 = new Point(5, 5);
+            Point point2 = new Point(5, 5);
+            Assert.assertEquals(distance(point1, point2), 0);
+        }
+
+        @Test
+        public void testPoint3() {
             Point point1 = new Point(1, 4);
             Point point2 = new Point(2, 7);
             Assert.assertEquals(distance(point1, point2), 4.1622776601683795);
