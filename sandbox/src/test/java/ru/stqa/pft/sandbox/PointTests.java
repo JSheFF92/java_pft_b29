1666e8eee1f72a18302fec194f494cc58167a56c
JSheFF92 <shevchuk_1992@list.ru>
2021-11-24 21:44:44 +0300

    Переделал тесты на основании переработки задачи

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,28 +3,24 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static ru.stqa.pft.sandbox.Distance.distance;
+
 
 
     public class PointTests {
         @Test
         public void testPoint1() {
-            Point point1 = new Point(1, 4);
-            Point point2 = new Point(2, 7);
-            Assert.assertEquals(distance(point1, point2), 3.1622776601683795);
+
+            Assert.assertEquals(PointDistance.distance(new PointDistance.Point(10,10), new PointDistance.Point(20,20)), 14.142135623730951);
         }
 
         @Test
         public void testPoint2() {
-            Point point1 = new Point(5, 5);
-            Point point2 = new Point(5, 5);
-            Assert.assertEquals(distance(point1, point2), 0);
+            Assert.assertEquals(PointDistance.distance(new PointDistance.Point(5,5), new PointDistance.Point(5,5)), 0);
         }
 
         @Test
         public void testPoint3() {
-            Point point1 = new Point(1, 4);
-            Point point2 = new Point(2, 7);
-            Assert.assertEquals(distance(point1, point2), 3.1622776601683795);
+
+            Assert.assertEquals(PointDistance.distance(new PointDistance.Point(1,4), new PointDistance.Point(2,7)), 3.1622776601683795);
         }
     }
