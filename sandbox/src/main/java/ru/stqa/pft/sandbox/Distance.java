01bb1b596aee5a73281702158828aa1af65849f5
JSheFF92 <shevchuk_1992@list.ru>
2021-11-24 21:42:55 +0300

    Добавил пункт 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -1,27 +1,14 @@
 package ru.stqa.pft.sandbox;
 
+import static ru.stqa.pft.sandbox.PointDistance.distance;
+
 public class Distance {
 
-    public static void main(String[] args) {
-        hello("Алексей");
+        public static void main(String[] args) {
 
-        Point point1 = new Point(1, 4);
-        Point point2 = new Point(2, 7);
-
-        System.out.println("Расстояние между двумя точками с координатами " + point1.x + ", " + point1.y + " и " + point2.x + ", " + point2.y + " = " + distance(point1, point2));
-
+            //не внутри функции задавать параметры, а отдельно. вызывая эту функцию) .distance()
+            System.out.println("Площадь прямоугольника равна " +
+                    distance(new PointDistance.Point(10,10), new PointDistance.Point(20,20))
+            );
+        }
     }
-
-    public static double distance(Point point1, Point point2) {
-        double dx = point1.x - point2.x;
-        double dy = point1.y - point2.y;
-        double r = Math.pow(dx, 2) + Math.pow(dy, 2);
-        return Math.sqrt(r);
-
-    }
-
-    public static void hello(String name) {
-        System.out.println("Здравствуйте, "+ name + "!");
-
-    }
-}
