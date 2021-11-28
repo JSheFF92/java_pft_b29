b46a9ef2909d57abd8208ca791858e322c6e5395
JSheFF92 <shevchuk_1992@list.ru>
2021-11-28 12:01:39 +0300

    2.3 Фикстуры. Доработка тест-кейса по лекции

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/GroupCreatedTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/GroupCreatedTests.java
@@ -2,7 +2,6 @@ package re.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -12,58 +11,49 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreatedTests {
-  private WebDriver wb;
+  private WebDriver wd;
 
 
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
 
-    wb = new ChromeDriver();
-    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
-  public void testGroupCreation() throws Exception {
-    wb.get("http://localhost/addressbook/");
-    wb.findElement(By.name("user")).clear();
-    wb.findElement(By.name("user")).sendKeys("admin");
-    wb.findElement(By.name("pass")).clear();
-    wb.findElement(By.name("pass")).sendKeys("secret");
-    wb.findElement(By.xpath("//input[@value='Login']")).click();
-    wb.findElement(By.linkText("groups")).click();
-    wb.findElement(By.name("new")).click();
-    wb.findElement(By.name("group_name")).click();
-    wb.findElement(By.name("group_name")).clear();
-    wb.findElement(By.name("group_name")).sendKeys("test1");
-    wb.findElement(By.name("group_header")).click();
-    wb.findElement(By.name("group_header")).clear();
-    wb.findElement(By.name("group_header")).sendKeys("test2");
-    wb.findElement(By.name("group_footer")).click();
-    wb.findElement(By.name("group_footer")).clear();
-    wb.findElement(By.name("group_footer")).sendKeys("test3");
-    wb.findElement(By.name("submit")).click();
-    wb.findElement(By.linkText("groups")).click();
-    wb.findElement(By.linkText("Logout")).click();
+  public void testGroupCreation() {
+    wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.name("new")).click();
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys("test1");
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys("test3");
+    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wb.quit();
+  public void tearDown() {
+    wd.quit();
   }
 
-  private boolean isElementPresent(By by) {
+  private boolean isAlertPresent(ChromeDriver wd) {
     try {
-      wb.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      wb.switchTo().alert();
+      wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
