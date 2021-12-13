6b3bbc605f8bbaeb2873e32dc192df6edf964b9d
JSheFF92 <shevchuk_1992@list.ru>
2021-12-13 21:47:24 +0300

    Лекция 4.6. Сравнение коллекций

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package re.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class ContactData {
     private final String name;
     private final String lastname;
@@ -17,6 +19,7 @@ public class ContactData {
         this.email = email;
         this.group = group;
     }
+
 
     public String getName() {
         return name;
@@ -40,5 +43,25 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/GroupDelitionTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/GroupDelitionTests.java
@@ -16,11 +16,14 @@ public class GroupDelitionTests extends TestBase {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
-
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToHomeGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
+
     }
 }
