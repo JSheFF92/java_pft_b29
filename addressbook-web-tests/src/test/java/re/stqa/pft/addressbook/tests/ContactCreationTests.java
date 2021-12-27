7e5a939a7acef651df486d010e4ed90e6ac8af56
JSheFF92 <shevchuk_1992@list.ru>
2021-12-28 01:21:51 +0300

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,28 +1,41 @@
 package re.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import re.stqa.pft.addressbook.model.ContactData;
 import re.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreated() {
-        app.goTo().ContactMDPage();
-        Contacts before = app.contact().all();
-        app.goTo().ContactPage();
-        File photo = new File("src/test/resources/stru.png");
-        ContactData contact = new ContactData().withFirstname("aaa").withLastname("bbb").withGroup("[none]").withPhoto(photo);
-        app.contact().create(contact);
-        assertThat(app.contact().count(), equalTo(before.size() + 1));
-        Contacts after = app.contact().all();
-        assertThat(after, equalTo(
-                before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {"test1", "lastName 1"});
+        list.add(new Object[] {"test2", "lastName 2"});
+        list.add(new Object[] {"test3", "lastName 3"});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreated(String firstName, String lastName) {
+            File photo = new File("src/test/resources/stru.png");
+            ContactData contact = new ContactData().withFirstname(firstName).withLastname(lastName).withGroup("[none]").withPhoto(photo);
+            app.goTo().ContactMDPage();
+            Contacts before = app.contact().all();
+            app.goTo().ContactPage();
+            app.contact().create(contact);
+            assertThat(app.contact().count(), equalTo(before.size() + 1));
+            Contacts after = app.contact().all();
+            assertThat(after, equalTo(
+                    before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
 
     @Test(enabled = false)
