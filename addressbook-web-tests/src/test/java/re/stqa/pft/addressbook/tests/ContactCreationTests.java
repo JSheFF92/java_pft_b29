67cc610bcb20c4a6e2709f055d87c585c4c10e18
JSheFF92 <shevchuk_1992@list.ru>
2021-12-29 00:21:51 +0300

    Лекция 6.6. Формат XML

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package re.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import re.stqa.pft.addressbook.model.ContactData;
@@ -9,9 +10,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -20,17 +21,18 @@ public class ContactCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src/test/resources/stru.png");
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
         String line = reader.readLine();
-        line.split(";");
         while (line != null){
-            String[] split = line.split(";");
-            list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withGroup("[none]").withPhoto(photo)});
+            xml += line;
             line = reader.readLine();
         }
-        return list.iterator();
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validContacts")
