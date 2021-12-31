9cbe7bcb3ab99c58d707292ef126adb759049abb
JSheFF92 <shevchuk_1992@list.ru>
2021-12-31 12:50:38 +0300

    Лекция 7.1. Драйвер базы данных. Получение данных из БД

---
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,66 @@
+package re.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import re.stqa.pft.addressbook.model.ContactData;
+import re.stqa.pft.addressbook.model.Contacts;
+import re.stqa.pft.addressbook.model.GroupData;
+import re.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+    @Test(enabled = false)
+    public void testDbconnectionGroup() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+            Groups groups = new Groups();
+            ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer  from group_list");
+            while (rs.next()) {
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name")).
+                        withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+
+            System.out.println(groups);
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    @Test
+    public void testDbconnectionContact() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+
+            Contacts contacts = new Contacts();
+            ResultSet c = st.executeQuery("select id, firstname, lastname, address from addressbook");
+
+            while (c.next()) {
+                contacts.add(new ContactData().withId(c.getInt("id")).withFirstname(c.getString("firstname")).
+                        withLastname(c.getString("lastname")).withAddress(c.getString("address")));
+            }
+            c.close();
+            st.close();
+            conn.close();
+            System.out.println(contacts);
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
