e047a3e076bc823d30125f5c1eac7bb223d7d869
JSheFF92 <shevchuk_1992@list.ru>
2022-01-05 14:12:52 +0300

    Лекция 7.5. Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -67,6 +67,7 @@ public class ContactCreationTests extends TestBase {
         Contacts after = app.db().contacts();
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        verifyContactListInUI();
     }
 
     @Test(enabled = false)
@@ -83,5 +84,6 @@ public class ContactCreationTests extends TestBase {
         assertThat(app.contact().count(), equalTo(before.size()));
         Contacts after = app.db().contacts();
         assertThat(after, equalTo(before));
+        verifyContactListInUI();
     }
 }
--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -63,6 +63,7 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        verifyGroupListInUI();
     }
 
     @Test(enabled = false)
@@ -78,5 +79,6 @@ public class GroupCreationTests extends TestBase {
         assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.db().groups();
         assertThat(after, equalTo(before));
+        verifyGroupListInUI();
     }
 }
