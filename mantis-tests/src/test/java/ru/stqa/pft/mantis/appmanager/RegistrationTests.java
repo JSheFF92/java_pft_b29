c7197474462d0d47acac0818f2f6c0d40617b4d2
JSheFF92 <shevchuk_1992@list.ru>
2022-01-13 01:29:32 +0300

    Лекция 8.4. Ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.tests.TestBase;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+
+    public void testRegistration(){
+        app.registration().start("user1", "user1@localhost.localdomain");
+
+    }
+}
