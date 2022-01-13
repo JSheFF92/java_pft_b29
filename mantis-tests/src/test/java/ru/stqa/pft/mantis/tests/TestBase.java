59ca6bde77a14c8160529ab3b884c20248030407
JSheFF92 <shevchuk_1992@list.ru>
2022-01-13 21:24:46 +0300

    ошибка

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,13 +5,15 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
     protected static final ApplicationManager app
             = new ApplicationManager (System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
-    public void setUp() throws Exception {
+    public void setUp() throws IOException {
         app.init();
     }
 
