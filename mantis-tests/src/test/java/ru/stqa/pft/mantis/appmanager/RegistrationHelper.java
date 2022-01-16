cc04b57a3ca88093c25ba74bde4f070703505f8e
JSheFF92 <shevchuk_1992@list.ru>
2022-01-16 23:12:14 +0300

    ошибка

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -21,4 +21,24 @@ public class RegistrationHelper extends HelperBase{
         type(By.name("password_confirm"), password);
         click(By.cssSelector("input[value='Изменить учетную запись']"));
     }
+
+    public void adminEnter(String username, String password){
+        wd.get(app.getProperty("web.baseUrl") + "/login_page.php");
+        type(By.name("username"), username);
+        type(By.name("password"), password);
+        click(By.cssSelector("input[value='Войти']"));
+    }
+
+    public void goToUserPage(){
+        wd.get(app.getProperty("web.baseUrl") + "/manage_user_page.php");
+        click(By.xpath("//*[@id=\"menu-items\"]/li[6]/a"));
+        click(By.xpath("//*[@id=\"manage-menu\"]/ul/li[1]/a"));
+        click(By.xpath("//a[@href='manage_user_edit_page.php?user_id=2']"));
+    }
+
+    public void ResetPassword(){
+        wd.get(app.getProperty("web.baseUrl") + "/manage_user_edit_page.php?user_id=2");
+        click(By.xpath("//input[@value='Сбросить пароль']"));
+    }
+
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/WorkRegistration.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/WorkRegistration.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
@@ -10,9 +12,9 @@ import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
-public class RegistrationTests extends TestBase {
+public class WorkRegistration extends TestBase{
 
-    //@BeforeMethod
+    @BeforeMethod
     public void startMailServer(){
         app.mail().start();
     }
@@ -20,15 +22,23 @@ public class RegistrationTests extends TestBase {
     @Test
     public void testRegistration() throws IOException, MessagingException {
 
+        app.registration().adminEnter("Administrator", "root");
+        app.registration().goToUserPage();
+        app.registration().ResetPassword();
+
         long now = System.currentTimeMillis();
-        String user = String.format("user%s", now);
-        String password = "password";
-        String email = String.format("user%s@localhost", now);
-        app.james().createUser(user, password);
-        app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+
+
+        String email = String.format("user1@localhost.localdomain", now);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
+
+        String user = String.format("user1", now);
+        String password = "password";
+
+
         app.registration().finish(confirmationLink, password);
+
         assertTrue(app.newSession().login(user, password));
     }
 
@@ -38,7 +48,7 @@ public class RegistrationTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-    //@AfterMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void stopMailServer(){
         app.mail().stop();
     }
