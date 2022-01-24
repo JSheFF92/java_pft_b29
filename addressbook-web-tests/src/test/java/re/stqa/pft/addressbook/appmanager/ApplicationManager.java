d071733e465947a96fa5b186df339d1510a43447
JSheFF92 <shevchuk_1992@list.ru>
2022-01-24 22:09:51 +0300

    10.3 промежуточный

--- addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/re/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package re.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -50,9 +51,10 @@ public class ApplicationManager {
             } else if (browser.equals(BrowserType.IE)) {
                 wd = new InternetExplorerDriver();
             }
-        } else{
+        } else {
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
             wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.get(properties.getProperty("web.baseUrl"));
@@ -79,7 +81,7 @@ public class ApplicationManager {
         return navigationHelper;
     }
 
-    public DbHelper db(){
+    public DbHelper db() {
         return dbHelper;
     }
 }
